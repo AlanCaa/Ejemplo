a02d67c0a91d8a498dfdfde068a8d981fb54430d
AlanCaa <89029660+AlanCaa@users.noreply.github.com>
2021-08-17 02:20:29 -0500

    Add files via upload

---
+++ Practica 1/src/practica/pkg1/Practica1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg1;
+import.java.util.Scanner
+/**
+ *
+ * @author ajosu
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scnCliente = new Scanner(System.in)
+        System.out.print("Nombre Cliente");
+        String cliente = scnCliente.nextLine();
+        
+        Cuenta cta = new Cuenta();
+        Cuenta cta = new Cuenta("Jorge", "ABC123", .10, 6089.33);
+        
+        System.out
+    }
+    
+}
